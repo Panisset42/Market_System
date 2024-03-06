fdbd132b05552eec92e56317de4830dc468d189e
Panisset42 <lucas.lima319@etec.sp.gov.br>
2024-03-05 22:34:35 -0300

    Just setting the first seed for this project

---
+++ product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Market_System
+{
+    internal class Product
+    {
+        public string ProductName { get; }
+        public int ProductCode { get; }
+        public float ProductPrice { get; }
+        public int ProductAmount { get; }
+
+        public Product(string productName, int productCode, float productPrice, int productAmount)
+        {
+            ProductName = productName;
+            ProductCode = productCode;
+            ProductPrice = productPrice;
+            ProductAmount = productAmount;
+        }
+    }
+}
---
+++ worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Market_System
+{
+    internal class Worker
+    {
+        public string WorkerName { get; }
+        public string PhoneNumber { get; }
+        public string Role { get; }
+        public DateTime LeaveDate { get; }
+        public DateTime EntryDate { get; }
+
+        // Constructor for required properties
+        public Worker(string workerName, string phoneNumber, string role, DateTime entryDate, DateTime leaveDate)
+        {
+            WorkerName = workerName;
+            PhoneNumber = phoneNumber;
+            Role = role;
+            EntryDate = entryDate;
+            LeaveDate = leaveDate;
+        }
+    }
+}
